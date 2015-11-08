08d3f573942e48926e9168fb297d295407e14565
Jason Kelly <jkelly09@gmail.com>
2015-11-07 19:20:29 -0800

    added link list

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 var koa = require('koa');
 var router = require('koa-router')();
 
-var data = require('./device-data.js');
+var data = require('./plug-data.js');
 
 var app = module.exports = koa();
 
-// GET /books -> List all the books in JSON.
-// GET /books/:id -> Returns the book for the given ID
-// POST /books/ -> JSON data to inserted to the books db.
-// PUT /books/:id -> JSON data to update the book data.
-// DELETE /books/:id -> Removes the book with the specified ID.
 // OPTIONS / -> Gives the list of allowed request types.
 // HEAD / -> HTTP headers only, no body.
 // TRACE / -> Blocked for security reasons.
-router.get('/devices', function* (){
-    this.body = yield data.devices.get();
+router.get('/', function* () {
+  this.body = {
+    'links' : [
+      'plugs' : this.request.origin + '/plugs'
+    ]
+  };
 });
-router.get('/devices/:id', function* (){
-    this.body = yield data.devices.get();
+
+// GET /plugs -> List all the books in JSON.
+// GET /plugs/:id -> Returns the book for the given ID
+// POST /plugs/ -> JSON data to inserted to the books db.
+// PUT /plugs/:id -> JSON data to update the book data.
+// DELETE /plugs/:id -> Removes the book with the specified ID.
+router.get('/plugs', function* (){
+    this.body = yield data.plugs.get();
+});
+router.get('/plugs/:id', function* (){
+    this.body = yield data.plugs.get();
 });
 
 app.use(router.routes());
