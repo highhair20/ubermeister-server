bdce228697937dff3a231b0afcbaa2ff070c72b5
Jason Kelly <jkelly09@gmail.com>
2015-11-09 10:58:36 -0800

    add plug controller and use mongo

---
+++ controllers/plugs.js
@@ -0,0 +1,47 @@
+'use strict';
+// var views = require('co-views');
+var parse = require('co-body');
+var monk = require('monk');
+var wrap = require('co-monk');
+// var db = monk('localhost/powerbank');
+var db = monk('ds053164.mongolab.com:53164/heroku_03lb5q27');
+var co = require('co');
+
+var plugs = wrap(db.get('plugs'));
+
+// From lifeofjs
+co(function * () {
+  var plugs = yield plugs.find({});
+});
+
+module.exports.home = function * home(next) {
+  if ('GET' != this.method) return yield next;
+  var domain = this.request.origin;
+  this.body = {
+    'links' : [{
+      'plugs' : domain.concat('/plugs')
+    }]
+  };
+};
+
+module.exports.all = function * all(next) {
+  if ('GET' != this.method) return yield next;
+  this.body = yield plugs.find({});
+};
+
+module.exports.add = function * add(data,next) {
+  if ('POST' != this.method) return yield next;
+  var plug = yield parse(this, {
+    limit: '1kb',
+    strict: false
+  });
+  console.log('position: ', plug.position);
+  console.log('mode: ', plug.mode);
+  var inserted = yield plugs.insert(plug);
+  if (!inserted) {
+    this.throw(405, "The plug couldn't be added.");
+  }
+  this.body = {
+    'status': 0
+  };
+};
--- index.js
+++ index.js
@@ -1,33 +1,43 @@
+'use strict';
+var plugs = require('./controllers/plugs.js');
+var compress = require('koa-compress');
+var logger = require('koa-logger');
 var koa = require('koa');
-var router = require('koa-router')();
-
-var data = require('./plug-data.js');
-
+var route = require('koa-route');
 var app = module.exports = koa();
 
+// Logger
+app.use(logger());
+
+// GET / -> List all links available.
 // OPTIONS / -> Gives the list of allowed request types.
 // HEAD / -> HTTP headers only, no body.
 // TRACE / -> Blocked for security reasons.
-router.get('/', function* () {
-  this.body = {
-    'links' : [
-      'plugs' : this.request.origin + '/plugs'
-    ]
-  };
-});
+app.use(route.get('/', plugs.home));
 
 // GET /plugs -> List all the books in JSON.
-// GET /plugs/:id -> Returns the book for the given ID
+app.use(route.get('/plugs/', plugs.all));
+
 // POST /plugs/ -> JSON data to inserted to the books db.
+app.use(route.post('/plugs/', plugs.add));
+
+// GET /plugs/:id -> Returns the book for the given ID
+
 // PUT /plugs/:id -> JSON data to update the book data.
 // DELETE /plugs/:id -> Removes the book with the specified ID.
-router.get('/plugs', function* (){
-    this.body = yield data.plugs.get();
-});
-router.get('/plugs/:id', function* (){
-    this.body = yield data.plugs.get();
-});
 
-app.use(router.routes());
 
+
+// router.get('/plugs', function* (){
+//     this.body = yield data.plugs.get();
+// });
+// router.get('/plugs/:id', function* (){
+//     this.body = yield data.plugs.get();
+// });
+// app.use(router.routes());
+
+// Compress
+app.use(compress());
+
+// listen
 app.listen(process.env.PORT || 3000);
